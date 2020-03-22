9fe2655022512538e59948d336bf9d0fbc1e67b4
shyam98 <shyam98@gmail.com>
2020-03-21 18:38:54 -0700

    switched std to array

--- 2D lander/Dynamic_simulation_lander_2D_4_fn.m
+++ 2D lander/Dynamic_simulation_lander_2D_4_fn.m
@@ -376,13 +376,13 @@ for q = q_range
         mean_sig_b_min(aa) = mean(sigma_bar_min_n(1:aa))/1e8;
         
         if aa>10
-            std_mean_g = std(mean_g(:,end-5:end));
-            std_mean_sig_ss_max = std(mean_sig_ss_max(:,end-5:end));
-            std_mean_sig_si_max = std(mean_sig_si_max(:,end-5:end));
-            std_mean_sig_ss_min = std(mean_sig_ss_min(:,end-5:end));
-            std_mean_sig_si_min = std(mean_sig_si_min(:,end-5:end));
-            std_mean_sig_b_max = std(mean_sig_b_max(:,end-5:end));
-            std_mean_sig_b_min = std(mean_sig_b_min(:,end-5:end));
+            std_mean_g(aa) = std(mean_g(:,end-5:end));
+            std_mean_sig_ss_max(aa) = std(mean_sig_ss_max(:,end-5:end));
+            std_mean_sig_si_max(aa) = std(mean_sig_si_max(:,end-5:end));
+            std_mean_sig_ss_min(aa) = std(mean_sig_ss_min(:,end-5:end));
+            std_mean_sig_si_min(aa) = std(mean_sig_si_min(:,end-5:end));
+            std_mean_sig_b_max(aa) = std(mean_sig_b_max(:,end-5:end));
+            std_mean_sig_b_min(aa) = std(mean_sig_b_min(:,end-5:end));
         end
         
         
@@ -398,8 +398,8 @@ for q = q_range
         
         
     end
-    mean_g
-    std_mean_g
+    mean_g;
+    std_mean_g;
     if aa>10
         figure()
         plot(1:aa, std_mean_g)
